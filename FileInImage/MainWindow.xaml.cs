8f63f3f14843c7dd6b7338a2592ea8d2590d55ae
skim <magiclogy@gmail.com>
2017-03-23 22:30:43 +0800

    Simple Implement

--- FileInImage/MainWindow.xaml.cs
+++ FileInImage/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Windows;
@@ -33,6 +34,18 @@ namespace FileInImage
         private void SynthesisButton_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
             SynthesisButton.Margin = new Thickness(0, 1, 0, 15);
+
+            if (file != null && img != null)
+            {
+                var output = File.OpenWrite(file.DirectoryName + "\\" + file.Name + "_combined" + img.Extension);
+                var f = file.OpenRead();
+                var i = img.OpenRead();
+                i.CopyTo(output);
+                f.CopyTo(output);
+                i.Close();
+                f.Close();
+                output.Close();
+            }
         }
         private void SynthesisButton_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
         {
@@ -55,5 +68,67 @@ namespace FileInImage
             base.OnClosed(e);
             Application.Current.Shutdown();
         }
+
+        private void FileDrop(object sender, DragEventArgs e)
+        {
+            if (!e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                return;
+            }
+            
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            
+            if (files.Length > 0)
+            {
+                try
+                {
+                    file = new FileInfo(files[0]);
+                    fileSize.Content = fileSize2Text(file.Length);
+                }
+                catch (Exception)
+                {
+                    file = null;
+                }
+            }
+        }
+
+        private string fileSize2Text(double size)
+        {
+            int level = 0;
+            string[] suffixs = new string[4] { "B", "KB", "MB", "GB" };
+            while (size >= 1024 && level <= 2)
+            {
+                level++;
+                size /= 1024;
+            }
+
+            return level == 0 ? size + "B" : String.Format("{0:0.00}", size) + suffixs[level];
+        }
+
+        private void ImageDrop(object sender, DragEventArgs e)
+        {
+            if (!e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                return;
+            }
+            
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+
+            if (files.Length > 0)
+            {
+                try
+                {
+                    img = new FileInfo(files[0]);
+                    imageSize.Content = fileSize2Text(img.Length);
+                }
+                catch (Exception)
+                {
+                    file = null;
+                }
+            }
+        }
+
+        private FileInfo file=null;
+        private FileInfo img=null;
     }
 }
